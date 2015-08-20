e532d5eb35341de5b1cf84d881d1401a24c3a541
Patrick Hufford <pahuff@microsoft.com>
2015-08-20 15:12:54 -0700

    Initial commit

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'realtekhdaudio'
+$installerType = 'EXE'
+$url = 'http://12244.wpc.azureedge.net/8012244/drivers/rtdrivers/pc/audio/0006-32bit_Win7_Win8_Win81_Win10_R279.exe'
+$url64 = 'http://12244.wpc.azureedge.net/8012244/drivers/rtdrivers/pc/audio/0006-64bit_Win7_Win8_Win81_Win10_R279.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
